7435893cc9e0c78a2f77364fba2011d8445cc59c
Kaibin <diaokaibin@gmail.com>
2020-03-09 20:12:11 +0800

    update

--- app/src/main/java/com/one/hook/MainActivity.java
+++ app/src/main/java/com/one/hook/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        startActivity();
+
         TextView tv = (TextView) findViewById(R.id.tv);
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
