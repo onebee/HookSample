7d9a3540b985d51da1079cd84ca9938060908627
Kaibin <diaokaibin@gmail.com>
2020-03-09 21:00:27 +0800

    update

---
+++ architect/src/main/java/com/one/architect/chart/ChargeAdapter.java
@@ -0,0 +1,18 @@
+package com.one.architect.chart;
+
+/**
+ * @author diaokaibin@gmail.com on 2020/3/9.
+ */
+public class ChargeAdapter extends ChinaCharge {
+    private USACharge usaCharge;
+
+
+    public ChargeAdapter(USACharge usaCharge) {
+        this.usaCharge = usaCharge;
+    }
+
+    @Override
+    public void chinaCharge() {
+
+    }
+}
---
+++ architect/src/main/java/com/one/architect/chart/USACharge.java
@@ -0,0 +1,8 @@
+package com.one.architect.chart;
+
+/**
+ * @author diaokaibin@gmail.com on 2020/3/9.
+ */
+public abstract class USACharge {
+    public abstract int usCharge();
+}
